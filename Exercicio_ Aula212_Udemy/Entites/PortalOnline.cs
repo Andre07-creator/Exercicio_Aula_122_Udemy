f49c1aed3f64ab3c48ae2dfd569d001313faf683
André Ramaldes <67028873+Andre07-creator@users.noreply.github.com>
2021-01-01 20:32:00 -0300

    Iniciando V1.0

---
+++ Exercicio_ Aula212_Udemy/Entites/PortalOnline.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio__Aula212_Udemy.Entites
+{
+    class PortalOnline
+    {
+        public SortedSet<int> A = new SortedSet<int>();
+        public SortedSet<int> B = new SortedSet<int>();
+        public SortedSet<int> C = new SortedSet<int>();
+        public void AddA(int a)
+        {
+            A.Add(a);
+        }
+        public void AddB(int b)
+        {
+            A.Add(b);
+        }
+        public void AddC(int c)
+        {
+            A.Add(c);
+        }
+        public override string ToString()
+        {
+            A.UnionWith(B);
+            A.UnionWith(C);
+            return "Total students: " + A.Count;
+        }
+    }   
+    
+}
---
+++ Exercicio_ Aula212_Udemy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Exercicio__Aula212_Udemy.Entites;
+using System.Collections.Generic;
+
+namespace Exercicio__Aula212_Udemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PortalOnline portal = new PortalOnline();
+            Console.Write("How many students for course A? ");
+            int na = int.Parse(Console.ReadLine());
+            for(int i = 0; i < na; i++)
+            {
+              portal.AddA (int.Parse(Console.ReadLine()));
+            }
+            Console.Write("How many students for course B? ");
+            int nb = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nb; i++)
+            {
+                portal.AddB(int.Parse(Console.ReadLine()));
+            }
+            Console.Write("How many students for course C? ");
+            int nc = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nc; i++)
+            {
+                portal.AddC(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(portal);
+        }
+
+    }
+}
